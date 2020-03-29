c1f5e0a06c993fcb2cc32a0188bdbc2f93992d91
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2020-03-29 12:09:59 +0200

    Added confirmation dialog for cherry-pick

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react"
 import {Octokit} from "@octokit/rest"
 import Commits from "./commits"
+import Confirm from "./confirm"
 import {pipe, none} from "ramda"
 import { formatISO, subDays } from 'date-fns'
 
@@ -10,62 +11,100 @@ const getAuthor = (commit) => commit.commit.author.name + "," + commit.commit.au
 const sameCommit = (c1, c2) =>
     c1.sha === c2.sha || getAuthor(c1) === getAuthor(c2)
 
-const fillMissing = ([dev, prod]) =>
-    [dev, [...dev.filter(c => none(cp => sameCommit(cp, c), prod)).map(c => ({...c, missing: true})), ...prod]]
+const fillMissing = ([from, to]) => ({
+    from,
+    to: [
+        ...from.filter(c => none(cp => sameCommit(cp, c), to)).map(c => ({...c, missing: true})),
+        ...to
+    ]
+})
 
 const replaceCommit = (commits, sha, commit) => commits.map(c => c.sha === sha ? commit : c)
     
 export default ({params = {}}) => {
-    const {owner, repo, from, to, token} = params
+    const {owner, repo, from, to, token, days} = params
     if (!token) {
-        return <div>Unautorized!</div>
+        return <div>Unautorized, please add token param!</div>
     }
     if (!owner || !repo || !from || !to) {
         return <div>owner, repo, from and to are mandatory params</div>
     }
+    const [error, setError] = useState(null)
     const octokit = new Octokit({
         auth: token
     })
     const [commits, setCommits] = useState(null)
-    const branches = [from, to]
+    const branches = {from, to}
     useEffect(() => {
-        Promise.all(branches.map(b => octokit.repos.listCommits({
+        Promise.all(["from", "to"].map(branch => octokit.repos.listCommits({
             owner,
             repo,
-            sha: b,
+            sha: branches[branch],
             per_page: 1000,
-            since: formatISO(subDays(new Date(), 60))
+            since: formatISO(subDays(new Date(), days || 60))
         })))
         .then((responses) => responses.map(c => c.data))
         .then(pipe(fillMissing, setCommits))
+        .catch(e => setError(e))
         
     }, [owner, repo, from, to])
     
     const goToIssue = (issue) => `https://github.com/${owner}/${repo}/issues/${issue}`
     const getHead = (commits) => commits.filter(c => !c.missing)[0].sha
-    const cherryPick = async (sha, commit) => {
-        const {author, committer, message, tree} = commit
-        const newCommit = await octokit.git.createCommit({
-            owner,
-            repo,
-            message,
-            tree: tree.sha,
-            author,
-            committer,
-            parents: [getHead(commits[1])]
-        })
-        await octokit.git.updateRef({
-            owner,
-            repo,
-            ref: `heads/${to}`,
-            sha: newCommit.data.sha,
-        })
-        debugger
-        setCommits([commits[0], replaceCommit(commits[1], sha, {...newCommit.data, commit})])
-    }
 
-    return commits && (<div className="bg-gray-700 p-6"><div className="text-xl text-white text-center">{owner}/{repo}</div><div className="flex">
-        {branches.map((b, idx) => <div className="flex-1" key={b}><Commits commits={commits[idx]} branch={b}
-            goToIssue={goToIssue} cherryPick={cherryPick}/></div>)}
-    </div></div>) || <div>Loading...</div>
+    const [confirm, setConfirm] = useState(null)
+
+    const withConfirm = (callback) => (...params) => {
+        setConfirm({
+            handler: () => {
+                callback(...params)
+                setConfirm(null)
+            },
+            cancel: () => {
+                setConfirm(null)
+            }
+        })
+    }
+    const cherryPick = withConfirm(async (sha, commit) => {
+        const {author, committer, message, tree} = commit
+        try {
+            const newCommit = await octokit.git.createCommit({
+                owner,
+                repo,
+                message,
+                tree: tree.sha,
+                author,
+                committer,
+                parents: [getHead(commits.to)]
+            })
+            await octokit.git.updateRef({
+                owner,
+                repo,
+                ref: `heads/${to}`,
+                sha: newCommit.data.sha,
+            })
+            setCommits({
+                from: commits.from, 
+                to: replaceCommit(commits.to, sha, {...newCommit.data, commit})
+            })
+        } catch(e) {
+            setError(e)
+        }
+    })
+
+    return commits && (
+        <div className="bg-gray-700 p-6">
+                <div className="text-xl text-white text-center">{owner}/{repo}</div>
+                <div className="flex">
+                    {["from", "to"].map(branch => <div className="flex-1" key={branch}><Commits commits={commits[branch]} branch={branch}
+                        goToIssue={goToIssue} cherryPick={cherryPick}/></div>)}
+                </div>
+                {confirm && 
+                    <Confirm message={"Do you want to cherry-pick the selected commit?"}
+                        onConfirm={confirm.handler}
+                        onCancel={confirm.cancel}
+                    />}
+                {error && <div>{error.message || error}</div>}
+        </div>
+    ) || <div>Loading...</div>
 }
